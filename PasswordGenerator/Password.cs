3bb6464751bbd47e7d3f04bdee4c4a191612c6c0
AzureAD\LucasLopes <Lucas.Lopes@AcceleratedFocus.com>
2022-07-11 09:04:25 -0700

    First version - we can create a random password with lower case, upper case, symbols and numbers

---
+++ PasswordGenerator/Password.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+
+namespace PasswordGenerator
+{
+    public class Password
+    {
+        public static string GetRandomPassword(int length, bool useLowerAlpha, bool useUpperAlpha, bool useSymbol)
+        {
+            const string lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+            const string upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string numbers = "0123456789";
+            const string symbols = "!@#$%^&*()_-+={}[]|:;<>,./?'";
+
+            string chars = numbers;
+            if (useLowerAlpha)
+            {
+                chars += lowerCaseLetters;
+            }
+            if (useSymbol)
+            {
+                chars += symbols;
+            }
+            if (useUpperAlpha)
+            {
+                chars += upperCaseLetters;
+            }
+
+            StringBuilder sb = new();
+            Random rnd = new();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rnd.Next(chars.Length);
+                switch (i)
+                {
+                    case 0:
+                        if (useLowerAlpha && length > 1)
+                        {
+                            int indexAlpha = rnd.Next(lowerCaseLetters.Length);
+                            sb.Append(lowerCaseLetters[indexAlpha]);
+                        }
+                        else
+                        {
+                            sb.Append(chars[index]);
+                        }
+                        break;
+                    case 1:
+                        if (useSymbol && length > 2)
+                        {
+                            int indexSymbol = rnd.Next(symbols.Length);
+                            sb.Append(symbols[indexSymbol]);
+                        }
+                        else
+                        {
+                            sb.Append(chars[index]);
+                        }
+                        break;
+                    case 2:
+                        if (useUpperAlpha && length > 3)
+                        {
+                            int indexCaseSensitive = rnd.Next(upperCaseLetters.Length);
+                            sb.Append(upperCaseLetters[indexCaseSensitive]);
+                        }
+                        else
+                        {
+                            sb.Append(chars[index]);
+                        }
+                        break;
+                    case 3:
+                        int indexnumbers = rnd.Next(numbers.Length);
+                        sb.Append(numbers[indexnumbers]);
+                        break;
+                    default:
+                        {
+                            sb.Append(chars[index]);
+                        }
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PasswordGenerator;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Password.GetRandomPassword(length: 50, useLowerAlpha:true, useSymbol:true,  useUpperAlpha:true));
+Console.WriteLine(Password.GetRandomPassword(length: 50, useLowerAlpha: true, useSymbol: true, useUpperAlpha: false));
+Console.WriteLine(Password.GetRandomPassword(length: 50, useLowerAlpha: true, useSymbol: false, useUpperAlpha: false));
+Console.WriteLine(Password.GetRandomPassword(length: 50, useLowerAlpha: false, useSymbol: false, useUpperAlpha: false));
+Console.WriteLine("\n");
+Console.WriteLine("\n");
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: true, useSymbol: true, useUpperAlpha: true));
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: true, useSymbol: true, useUpperAlpha: true));
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: true, useSymbol: true, useUpperAlpha: true));
+Console.WriteLine("\n");
+Console.WriteLine("\n");
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: false, useSymbol: true, useUpperAlpha: true));
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: false, useSymbol: true, useUpperAlpha: true));
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: false, useSymbol: true, useUpperAlpha: true));
+Console.WriteLine("\n");
+Console.WriteLine("\n");
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: false, useSymbol: false, useUpperAlpha: true));
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: false, useSymbol: false, useUpperAlpha: true));
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: false, useSymbol: false, useUpperAlpha: true));
+Console.WriteLine("\n");
+Console.WriteLine("\n");
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: false, useSymbol: false, useUpperAlpha: false));
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: false, useSymbol: false, useUpperAlpha: false));
+Console.WriteLine(Password.GetRandomPassword(length: 4, useLowerAlpha: false, useSymbol: false, useUpperAlpha: false));
+Console.WriteLine("\n");
+Console.WriteLine("\n");
+Console.WriteLine(Password.GetRandomPassword(length: 1, useLowerAlpha: false, useSymbol: false, useUpperAlpha: false));
+Console.WriteLine(Password.GetRandomPassword(length: 1, useLowerAlpha: true, useSymbol: false, useUpperAlpha: false));
+Console.WriteLine(Password.GetRandomPassword(length: 1, useLowerAlpha: true, useSymbol: true, useUpperAlpha: false));
+Console.WriteLine(Password.GetRandomPassword(length: 1, useLowerAlpha: true, useSymbol: true, useUpperAlpha: true));
+Console.WriteLine(Password.GetRandomPassword(length: 1, useLowerAlpha: false, useSymbol: true, useUpperAlpha: false));
+Console.WriteLine(Password.GetRandomPassword(length: 1, useLowerAlpha: false, useSymbol: false, useUpperAlpha: true));
+Console.WriteLine(Password.GetRandomPassword(length: 1, useLowerAlpha: false, useSymbol: true, useUpperAlpha: true));
+
+Console.ReadLine();
